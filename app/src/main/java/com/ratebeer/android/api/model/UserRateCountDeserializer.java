59e5ca614ddc871a954d33ec5803f90e3f9090f0
Eric Kok <erickok@gmail.com>
2016-03-15 08:37:53 +0100

    Handle case where the user has not 0 but null ratings when parsing rate counts to prevent login failure.

--- app/src/main/java/com/ratebeer/android/api/model/UserRateCountDeserializer.java
+++ app/src/main/java/com/ratebeer/android/api/model/UserRateCountDeserializer.java
@@ -3,6 +3,7 @@ package com.ratebeer.android.api.model;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.ratebeer.android.api.model.UserRateCount;
@@ -15,9 +16,12 @@ public final class UserRateCountDeserializer implements JsonDeserializer<UserRat
 	public UserRateCount deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
 		JsonObject object = json.getAsJsonObject();
 		UserRateCount userRateCount = new UserRateCount();
-		userRateCount.rateCount = object.get("RateCount").getAsInt();
-		userRateCount.placeCount = object.get("PlaceRatings").getAsInt();
-		userRateCount.tickCount = object.get("TickCount").getAsInt();
+		if (!(object.get("RateCount") instanceof JsonNull))
+			userRateCount.rateCount = object.get("RateCount").getAsInt();
+		if (!(object.get("PlaceRatings") instanceof JsonNull))
+			userRateCount.placeCount = object.get("PlaceRatings").getAsInt();
+		if (!(object.get("TickCount") instanceof JsonNull))
+			userRateCount.tickCount = object.get("TickCount").getAsInt();
 		return userRateCount;
 	}
 
